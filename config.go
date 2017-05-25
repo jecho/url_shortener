666ad82de3835734f835aa9d8ad3232dfb9b06c2
Jecho Ricafrente <jecho@foxley.co>
2017-05-24 21:02:12 -0700

    added config struct

---
+++ config.go
@@ -0,0 +1,8 @@
+package main
+
+type Config struct {
+	DB_HOST string
+	DB_NAME string
+	DB_USER string
+	DB_PASS string
+}
--- env.go
+++ env.go
@@ -6,6 +6,7 @@ import (
 
 // struct object
 type Env struct {
+	config *Config
 	db *sql.DB
 	//logger *log.Logger
 	//templates *template.Template
