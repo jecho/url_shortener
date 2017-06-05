c8d7219729cd4c9779a35aee2e81d4a3139e603d
Jecho Ricafrente <jecho@foxley.co>
2017-06-05 00:22:11 -0700

    updated tests for shortUrlShortner

--- simpleUrlShortner_test.go
+++ simpleUrlShortner_test.go
@@ -13,6 +13,7 @@ func TestEncode(t *testing.T) {
 		{"26", "A"},
 		{"51", "Z"},
 		{"61", "9"},
+		{"147", "cx"},
 	}{
 		n, _:= strconv.Atoi(c.in)
 		got := encode(n, base, a)
@@ -20,8 +21,24 @@ func TestEncode(t *testing.T) {
 			t.Errorf("Encode(%q) == %q, want %q", c.in, got, c.want)
 		}
 	}
+}
 
-
+func TestDecode(t *testing.T) {
+	for _, c:= range []struct {
+		in, want string
+	} {
+		{"b", "1"},
+		{"A", "26"},
+		{ "Z", "51"},
+		{ "9", "61"},
+		{ "cx", "147"},
+	}{
+		n := c.in
+		got := decode(n, a)
+		if strconv.Itoa(got) != c.want {
+			t.Errorf("Encode(%q) == %q, want %q", c.in, got, c.want)
+		}
+	}
 }
 
 func TestReverse(t *testing.T) {
